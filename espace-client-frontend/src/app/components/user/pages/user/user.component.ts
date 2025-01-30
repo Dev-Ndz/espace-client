905f507ffee218a3024d2f9e0d75c7408b164822
Ndzski <59318615+Dev-Ndz@users.noreply.github.com>
2025-01-30 22:07:36 +0100

    header

--- espace-client-frontend/src/app/components/user/pages/user/user.component.ts
+++ espace-client-frontend/src/app/components/user/pages/user/user.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './partials/navigation/navigation.component';
 import { ClientService } from '../../../../services/client.service';
 import { Client } from '../../../../models/client';
+import { HeaderComponent } from "./partials/header/header.component";
 
 @Component({
   selector: 'app-user',
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [RouterOutlet, NavigationComponent, HeaderComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
