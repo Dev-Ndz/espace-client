3cec14242adf4fdac4dcc9583781cab639baf464
Ndzski <59318615+Dev-Ndz@users.noreply.github.com>
2025-02-10 21:41:21 +0100

    test ci

--- espace-client-frontend/src/app/components/user/pages/user/partials/navigation/navigation.component.spec.ts
+++ espace-client-frontend/src/app/components/user/pages/user/partials/navigation/navigation.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavigationComponent } from './navigation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
@@ -8,9 +10,14 @@ describe('NavigationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavigationComponent]
-    })
-    .compileComponents();
+      imports: [NavigationComponent, BrowserAnimationsModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: { params: of({ id: '123' }) },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NavigationComponent);
     component = fixture.componentInstance;
