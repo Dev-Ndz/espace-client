c379ee6f801265156896eb719acd307311be3b0b
ndz <nicolas.dzierzgowski@gmail.com>
2025-02-18 20:24:14 +0100

    add admin side

---
+++ espace-client-frontend/src/app/components/admin/admin-page/admin-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-page',
+  imports: [RouterOutlet],
+  templateUrl: './admin-page.component.html',
+  styleUrl: './admin-page.component.scss',
+})
+export class AdminPageComponent {}
