707711f072ce5394626624cc96bace3071c60ab0
Ndzski <59318615+Dev-Ndz@users.noreply.github.com>
2025-03-07 18:29:32 +0100

    update cors origin config

--- espace-client-backend/src/main.ts
+++ espace-client-backend/src/main.ts
@@ -5,14 +5,21 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const corsOrigin =
+    process.env.NODE_ENV === 'production'
+      ? process.env.CORS_ORIGIN
+      : 'http://localhost:4200';
+
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:4200', // Remplacez "*" par l'origine spécifique pour plus de sécurité en production
+    origin: corsOrigin, // Remplacez "*" par l'origine spécifique pour plus de sécurité en production
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization', // Ajouter d'autres headers si nécessaire
   });
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
   console.log('listening to port: ', process.env.PORT ?? 3000);
+  console.log('allowed origin : ', corsOrigin);
 }
 bootstrap();
