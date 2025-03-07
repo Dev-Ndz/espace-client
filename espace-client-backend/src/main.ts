5fa781b323cdeb514829b3d5b5da79e3f01cd4bb
Ndzski <59318615+Dev-Ndz@users.noreply.github.com>
2025-03-07 19:53:22 +0100

    remove tests and correct backend url

--- espace-client-backend/src/main.ts
+++ espace-client-backend/src/main.ts
@@ -6,20 +6,20 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  const corsOrigin =
-    process.env.NODE_ENV === 'production'
-      ? process.env.CORS_ORIGIN
-      : 'http://localhost:4200';
+  const allowedOrigins = [
+    'http://localhost:4200',
+    'https://espace-client-ldz.netlify.app',
+  ];
 
-  app.setGlobalPrefix('api');
+  // app.setGlobalPrefix('api');
   app.enableCors({
-    origin: corsOrigin, // Remplacez "*" par l'origine spécifique pour plus de sécurité en production
+    origin: allowedOrigins, // Remplacez "*" par l'origine spécifique pour plus de sécurité en production
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization', // Ajouter d'autres headers si nécessaire
   });
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
   console.log('listening to port: ', process.env.PORT ?? 3000);
-  console.log('allowed origin : ', corsOrigin);
+  console.log('allowed origin : ', allowedOrigins);
 }
 bootstrap();
