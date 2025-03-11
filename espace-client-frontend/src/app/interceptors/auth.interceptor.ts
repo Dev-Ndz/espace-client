8d880f966409e2b1f416da44827f0ca5b739e164
Ndzski <59318615+Dev-Ndz@users.noreply.github.com>
2025-03-11 10:29:52 +0100

    debug interceptor

--- espace-client-frontend/src/app/interceptors/auth.interceptor.ts
+++ espace-client-frontend/src/app/interceptors/auth.interceptor.ts
@@ -4,5 +4,6 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const reqWithCredientials = req.clone({
     withCredentials: true,
   });
+  console.log('intercepting...');
   return next(reqWithCredientials);
 };
