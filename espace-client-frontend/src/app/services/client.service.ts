7f21c5e1e3f92210f1ffc294f96ed4f62a43b85f
Ndzski <59318615+Dev-Ndz@users.noreply.github.com>
2025-03-11 09:45:47 +0100

    modify API URL

--- espace-client-frontend/src/app/services/client.service.ts
+++ espace-client-frontend/src/app/services/client.service.ts
@@ -12,6 +12,9 @@ export class ClientService {
   clients = signal<Client[]>([]);
   client = signal<Client | undefined>(undefined);
 
+  CLIENT_API_URL = environment.apiUrl + '/client';
+  API_URL = environment.apiUrl;
+
   getConnectedClient(): void {
     this.http
       .get<Client>(environment.apiUrl + '/auth/me')
@@ -25,11 +28,11 @@ export class ClientService {
     if (client) {
       return of(client);
     } else {
-      return this.http.get<Client>(`${environment.apiUrl}/client/${clientId}`);
+      return this.http.get<Client>(`${this.CLIENT_API_URL}/${clientId}`);
     }
   }
   getAllClients() {
-    this.http.get<Client[]>(environment.apiUrl + '/client').subscribe({
+    this.http.get<Client[]>(this.CLIENT_API_URL).subscribe({
       next: (clients) => {
         this.clients.set(clients);
       },
@@ -37,7 +40,7 @@ export class ClientService {
     });
   }
   createClient(client: Client): Observable<Client> {
-    return this.http.post<Client>(environment.apiUrl + '/client', client);
+    return this.http.post<Client>(this.CLIENT_API_URL, client);
   }
   updateClient(client: Client, id: string): Observable<Client> {
     return this.http.patch<Client>(
@@ -46,11 +49,11 @@ export class ClientService {
     );
   }
   deleteClient(id: string): Observable<Client> {
-    return this.http.delete<Client>(environment.apiUrl + '/client/' + id);
+    return this.http.delete<Client>(this.CLIENT_API_URL + '/' + id);
   }
 
   addUser(clientId: string): Observable<{ url: string }> {
-    return this.http.post<{ url: string }>(environment.apiUrl + '/invitation', {
+    return this.http.post<{ url: string }>(this.API_URL + '/invitation', {
       clientId,
     });
   }
