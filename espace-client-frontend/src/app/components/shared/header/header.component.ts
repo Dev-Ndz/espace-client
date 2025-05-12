7987921ec9b0d149163872b4a0c77d1a279fc6ac
Ndzski <59318615+Dev-Ndz@users.noreply.github.com>
2025-05-12 21:04:57 +0200

    add admin or client component

--- espace-client-frontend/src/app/components/shared/header/header.component.ts
+++ espace-client-frontend/src/app/components/shared/header/header.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { AuthService } from '../../../services/auth.service';
+import { AdminOrUserComponent } from "./admin-or-user/admin-or-user.component";
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, ButtonModule],
+  imports: [RouterLink, ButtonModule, AdminOrUserComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
